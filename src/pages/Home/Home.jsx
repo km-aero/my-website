8651f8a2edb0bd539865b7c04519d3d00a925ba9
Kevin Monteiro <kevin.monteiro96@gmail.com>
2024-04-05 21:13:00 +0100

    feat: update page section styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
-import { Heading } from '@/components';
+import { Heading, Paragraph } from '@/components';
 
 export const Home = () => {
   return (
     <>
-      <div className='container mx-auto w-3/4 bg-slate-300'>
+      <div className='container w-3/4 bg-slate-300 my-10 px-5 py-1 rounded-3xl'>
         <Heading text='About Me' />
+        <Paragraph text='lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum.' />
+      </div>
+      <div className='container w-3/4 bg-slate-300 my-10 px-5 py-1 rounded-3xl'>
+        <Heading text='My Experience' />
+        <Paragraph text='lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum.' />
       </div>
     </>
   );
