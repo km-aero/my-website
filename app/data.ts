e3c18a0ef311b91ced40f1e8cbe9cc6e3508136f
Kevin Monteiro <kevin.monteiro96@gmail.com>
2025-05-24 23:28:55 +0100

    feat: add certification section

--- app/data.ts
+++ app/data.ts
@@ -16,6 +16,14 @@ type Education = {
   id: string
 }
 
+type Certification = {
+  company: string
+  title: string
+  issued: string
+  link: string
+  id: string
+}
+
 type BlogPost = {
   title: string
   description: string
@@ -106,10 +114,56 @@ export const EDUCATION: Education[] = [
   },
 ]
 
+export const CERTIFICATES: Certification[] = [
+  {
+    company: 'Cloud Native Computing Foundation',
+    title: 'Certified Kubernetes Administrator',
+    issued: 'Aug 2024',
+    link: 'https://www.credly.com/badges/d9b7516b-1879-4ef4-8f17-b9672931f48d',
+    id: 'cert-1',
+  },
+  {
+    company: 'Amazon Web Services',
+    title: 'DevOps Engineer - Professional',
+    issued: 'Nov 2023',
+    link: 'https://www.credly.com/badges/be53bd1d-fb3b-4b71-808f-cbec061d53d0',
+    id: 'cert-2',
+  },
+  {
+    company: 'HashiCorp',
+    title: 'Terraform Associate (003)',
+    issued: 'Jun 2023',
+    link: 'https://www.credly.com/badges/b6748eed-066c-43d5-8596-b03784ba8ad5',
+    id: 'cert-3',
+  },
+  {
+    company: 'Microsoft',
+    title: 'Azure Fundamentals',
+    issued: 'Mar 2023',
+    link: 'https://www.credly.com/badges/b888ebb3-5aa8-4e8f-acf8-fe50bf897161',
+    id: 'cert-4',
+  },
+  {
+    company: 'Amazon Web Services',
+    title: 'SysOps Administrator',
+    issued: 'Oct 2022',
+    link: 'https://www.credly.com/badges/6808b510-79b5-4383-9fde-a7cdd123126b',
+    id: 'cert-5',
+  },
+  {
+    company: 'Google',
+    title: 'Certified Associate Cloud Engineer',
+    issued: 'Oct 2022',
+    link: 'https://www.credly.com/badges/7daa3636-3c77-4bc3-bb4d-4144edbf30e6',
+    id: 'cert-6',
+  },
+]
+
 export const BLOG_POSTS: BlogPost[] = [
   {
     title: 'Creating and hosting my personal website',
-    description: 'How I started my online presence by using a vercel starter next js app.',
+    description:
+      'How I started my online presence by using a vercel starter next js app.',
     link: '/blog/my-personal-website',
     uid: 'blog-1',
   },
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import { AnimatedBackground } from '@/components/ui/animated-background'
 import {
   BLOG_POSTS,
+  CERTIFICATES,
   EDUCATION,
   EMAIL,
   SOCIAL_LINKS,
@@ -153,6 +154,44 @@ export default function Personal() {
                   </div>
                   <p className="text-zinc-600 dark:text-zinc-400">
                     {school.start} - {school.end}
+                  </p>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </motion.section>
+
+      <motion.section
+        variants={VARIANTS_SECTION}
+        transition={TRANSITION_SECTION}
+      >
+        <h3 className="mb-5 text-lg font-medium">Certification</h3>
+        <div className="flex flex-col space-y-2">
+          {CERTIFICATES.map((certificate) => (
+            <a
+              className="relative overflow-hidden rounded-2xl bg-zinc-300/30 p-[1px] dark:bg-zinc-600/30"
+              href={certificate.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={certificate.id}
+            >
+              <Spotlight
+                className="from-zinc-900 via-zinc-800 to-zinc-700 blur-2xl dark:from-zinc-100 dark:via-zinc-200 dark:to-zinc-50"
+                size={64}
+              />
+              <div className="relative h-full w-full rounded-[15px] bg-white p-4 dark:bg-zinc-950">
+                <div className="relative flex w-full flex-row justify-between">
+                  <div>
+                    <h4 className="font-normal dark:text-zinc-100">
+                      {certificate.company}
+                    </h4>
+                    <p className="text-zinc-500 dark:text-zinc-400">
+                      {certificate.title}
+                    </p>
+                  </div>
+                  <p className="text-zinc-600 dark:text-zinc-400">
+                    {certificate.issued}
                   </p>
                 </div>
               </div>
