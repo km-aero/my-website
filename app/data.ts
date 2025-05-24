27dad5b225682f1a504f20648ba549b44880c4d1
Kevin Monteiro <kevin.monteiro96@gmail.com>
2025-05-24 23:11:32 +0100

    feat: remove projects section and update existing sections

--- app/data.ts
+++ app/data.ts
@@ -1,11 +1,3 @@
-type Project = {
-  name: string
-  description: string
-  link: string
-  video: string
-  id: string
-}
-
 type WorkExperience = {
   company: string
   title: string
@@ -27,79 +19,71 @@ type SocialLink = {
   link: string
 }
 
-export const PROJECTS: Project[] = [
-  {
-    name: 'Motion Primitives Pro',
-    description:
-      'Advanced components and templates to craft beautiful websites.',
-    link: 'https://pro.motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
-    id: 'project1',
-  },
-  {
-    name: 'Motion Primitives',
-    description: 'UI kit to make beautiful, animated interfaces.',
-    link: 'https://motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
-    id: 'project2',
-  },
-]
-
 export const WORK_EXPERIENCE: WorkExperience[] = [
   {
-    company: 'Reglazed Studio',
-    title: 'CEO',
-    start: '2024',
+    company: 'Capgemini Invent',
+    title: 'Platform Engineer',
+    start: 'Oct 2024',
     end: 'Present',
-    link: 'https://ibelick.com',
+    link: 'https://www.capgemini.com/au-en/about-us/who-we-are/our-brands/capgemini-invent/',
     id: 'work1',
   },
   {
-    company: 'Freelance',
-    title: 'Design Engineer',
-    start: '2022',
-    end: '2024',
-    link: 'https://ibelick.com',
+    company: 'Capgemini Invent',
+    title: 'Technical Lead',
+    start: 'May 2023',
+    end: 'Oct 2024',
+    link: 'https://www.capgemini.com/au-en/about-us/who-we-are/our-brands/capgemini-invent/',
     id: 'work2',
   },
   {
-    company: 'Freelance',
-    title: 'Front-end Developer',
-    start: '2017',
-    end: 'Present',
-    link: 'https://ibelick.com',
+    company: 'Capgemini Invent',
+    title: 'Lead DevOps Engineer',
+    start: 'Nov 2022',
+    end: 'May 2023',
+    link: 'https://www.capgemini.com/au-en/about-us/who-we-are/our-brands/capgemini-invent/',
     id: 'work3',
+  },
+  {
+    company: 'Capgemini Invent',
+    title: 'DevOps Engineer',
+    start: 'Apr 2022',
+    end: 'Nov 2023',
+    link: 'https://www.capgemini.com/au-en/about-us/who-we-are/our-brands/capgemini-invent/',
+    id: 'work4',
+  },
+  {
+    company: 'Sparta Global',
+    title: 'DevOps Engineer',
+    start: 'Oct 2020',
+    end: 'Apr 2023',
+    link: 'https://www.spartaglobal.com/',
+    id: 'work5',
+  },
+  {
+    company: 'Sparta Global',
+    title: 'Trainee DevOps Engineer',
+    start: 'Feb 2020',
+    end: 'Oct 2020',
+    link: 'https://www.spartaglobal.com/',
+    id: 'work6',
+  },
+  {
+    company: 'Citco',
+    title: 'Credit & FX Analyst',
+    start: 'Sep 2019',
+    end: 'Feb 2020',
+    link: 'https://www.citco.com/',
+    id: 'work7',
   },
 ]
 
 export const BLOG_POSTS: BlogPost[] = [
   {
-    title: 'Exploring the Intersection of Design, AI, and Design Engineering',
-    description: 'How AI is changing the way we design',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+    title: 'Creating and hosting my personal website',
+    description: 'How I started my online presence by using a vercel starter next js app.',
+    link: '/blog/my-personal-website',
     uid: 'blog-1',
-  },
-  {
-    title: 'Why I left my job to start my own company',
-    description:
-      'A deep dive into my decision to leave my job and start my own company',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
-    uid: 'blog-2',
-  },
-  {
-    title: 'What I learned from my first year of freelancing',
-    description:
-      'A look back at my first year of freelancing and what I learned',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
-    uid: 'blog-3',
-  },
-  {
-    title: 'How to Export Metadata from MDX for Next.js SEO',
-    description: 'A guide on exporting metadata from MDX files to leverage Next.js SEO features.',
-    link: '/blog/example-mdx-metadata',
-    uid: 'blog-4',
   },
 ]
 
