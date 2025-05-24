242561b4bc5480474e69fa04b680ecbaeea925b8
Kevin Monteiro <kevin.monteiro96@gmail.com>
2025-05-24 23:39:16 +0100

    update website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,14 +14,14 @@ export const viewport: Viewport = {
 export const metadata: Metadata = {
   metadataBase: new URL('https://nim-fawn.vercel.app/'),
   alternates: {
-    canonical: '/'
+    canonical: '/',
   },
   title: {
-    default: 'Nim - Personal website template',
-    template: '%s | Nim'
+    default: 'Kevin Monteiro - Personal Website',
+    template: '%s | Nim',
   },
-  description:  'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
-};
+  description: 'Kevin is a DevOps engineer!',
+}
 
 const geist = Geist({
   variable: '--font-geist',
