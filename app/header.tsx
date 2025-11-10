3d10eae08bc883483bfb19a9e4c29b970e1c50d5
Kevin Monteiro <kevin.monteiro96@gmail.com>
2025-11-10 17:42:31 +0000

    fix: update job title in header

--- app/header.tsx
+++ app/header.tsx
@@ -16,7 +16,7 @@ export function Header() {
           className="text-zinc-600 dark:text-zinc-500"
           delay={0.5}
         >
-          DevOps Engineer
+          Senior DevOps Engineer
         </TextEffect>
       </div>
     </header>
